91f671de7eb79e7ae9c549a27e1d8fba03711ab8
demoGit <ngoduchungnaruto@gmail.com>
2019-08-26 17:52:24 +0700

    fix toan bo loi design

--- client/src/profiles/home/Home.style.tsx
+++ client/src/profiles/home/Home.style.tsx
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 
 export const Root = styled.div`
 background: white;
+padding-bottom:40px;
 `
 
 export const NameOfSkills = styled.div`
@@ -17,7 +18,9 @@ export const NameOfSkills = styled.div`
 
 export const Skill = styled.div`
 margin: 0px 70px;
-padding-bottom: 40px;
+@media (max-width:768px) {
+    margin:0px 20px;
+}
 `
 
 export const Name = styled.div`
@@ -30,6 +33,9 @@ width: 400px;
 height: 5px;
 border-radius: 10px;
 margin:10px 0 10px 0px;
+@media (max-width:768px) {
+    width:100%;
+}
 `
 
 export const Rate2 = styled.div`
@@ -38,6 +44,9 @@ height: 5px;
 border-radius: 10px;
 margin:10px 0 10px 0px;
 opacity:1;
+@media (max-width:768px) {
+    width:100px!important;
+}
 `
 
 // AboutMe.tsx
@@ -67,4 +76,56 @@ justify-content:center;
 align-items:center;
 color:#fff;
 box-Shadow:0.4px 2px 12px rgb(185, 181, 181);
+`
+
+// MyServices.tsx
+
+export const TitleMyServices = styled.div`
+font-weight: bolder;
+font-size: 20px;
+border-bottom:1px solid #00b4d9;
+width: 115px;
+margin-left:20px;
+padding-top:20px;
+`
+
+export const Services = styled.div`
+padding-bottom:30px;
+margin-left:13px;
+margin-right:13px;
+display:flex;
+flex-direction:row;
+@media (max-width:1024px) {
+    flex-wrap:wrap;
+}
+`
+
+export const Service = styled.div`
+background:white;
+width:155px;
+height:180px;
+border-radius:10px;
+margin:20px 10px;
+box-Shadow:0.4px 2px 12px rgb(185, 181, 181);
+@media (max-width:1024px) {
+width:200px;
+flex-grow:1;
+height:210px;
+}
+`
+
+export const Image = styled.img`
+margin:25px 20px 10px 15px;
+height: 50px;
+width:50px;
+`
+export const NameOfService = styled.div`
+margin:0px 15px;
+color:black;
+font-size: 14px;
+font-weight: 500;
+`
+
+export const Detail = styled.div`
+margin:0px 15px;
 `
--- client/src/profiles/home/index.tsx
+++ client/src/profiles/home/index.tsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React from 'react';
 import { Box } from '../../shared/Box';
 import AboutMe from './AboutMe';
 import Skills from './Skills';
 import TimeUpdate from './TimeUpdate';
+import MyServices from './MyServices';
+import img from './html.svg';
 
 const ProfileHome:React.FC = () => {
 
@@ -10,9 +12,10 @@ const ProfileHome:React.FC = () => {
 		<Box style = {{border: 'none',
 			borderRadius: '10px',
 			boxShadow:'0.4px 2px 12px rgb(185, 181, 181)'}}>
-			<AboutMe seftIntroduce='Started earnest brother believe an exposed so. Me he believing daughters if forfeited at furniture. Age again and stuff downs spoke. Late hour new nay able fat each sell. Nor themselves age introduced frequently use unsatiable devonshire get. They why quit gay cold rose deal park. One same they four did ask busy. Reserved opinions fat him nay position. Breakfast as zealously incommode do agreeable furniture. One too nay led fanny allow plate. 
+			<AboutMe seftIntroduce='Started earnest brother believe an exposend so. Me he believing daughters if forfeited at furniture. Age again and stuff downs spoke. Late hour new nay able fat each sell. Nor themselves age introduced frequently use unsatiable devonshire get. They why quit gay cold rose deal park. One same they four did ask busy. Reserved opinions fat him nay position. Breakfast as zealously incommode do agreeable furniture. One too nay led fanny allow plate. 
 
 Quick six blind smart out burst. Perfectly on furniture dejection determine my depending an to. Add short water court fat. Her bachelor honoured perceive securing but desirous ham required. Questions deficient acuteness to engrossed as. Entirely led ten humoured greatest and yourself. Besides ye country on observe. She continue appetite endeavor she judgment interest the met. For she surrounded motionless fat resolution may.' />
+			<MyServices services = {[{name: 'Web Development', detail: 'vừa học đc 2 tháng một con vit xòe ra 2 cái cánh', image:img},{name: 'Web Development', detail: 'vừa học đc 2 tháng một con vit xòe ra 2 cái cánh', image:img},,{name: 'Web Development', detail: 'vừa học đc 2 tháng một con vit xòe ra 2 cái cánh', image:img},,{name: 'Web Development', detail: 'vừa học đc 2 tháng một con vit xòe ra 2 cái cánh', image:img}]}></MyServices>
 			<Skills nameOfSkills = {'Code Skill'} skills = {[{name : 'javascript',rate : '300px'},{name : 'html',rate : '200px'},{name : 'CSS',rate : '100px'}]} />
 			<Skills nameOfSkills = {'Code Skill'} skills = {[{name : 'javascript',rate : '300px'},{name : 'html',rate : '200px'},{name : 'CSS',rate : '100px'}]} />
 		</Box>
