2b4161e7d011aa232f9a7a9bab0206ecfd7f331c
Nguyen Viet Canh <canhcvp1998@gmail.com>
2019-11-27 20:39:18 +0700

    remote package server

--- packages/server/services/user/src/app.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import routeHandler from './routes';
-
-const app = express();
-
-app.use(express.static(`${__dirname}/../assets`));
-app.use('/', routeHandler);
-
-export default app;
